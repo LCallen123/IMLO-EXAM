fc34c1661a6531f1661355b04c9db099677d4171
Luke Callen <kgj508@york.ac.uk>
2025-05-09 15:08:04 +0100

    created simple neural network

--- IMLO.py
+++ IMLO.py
@@ -1,11 +1,14 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import torchvision
 import torchvision.transforms as transforms
 
 # Transforms the dataset
-transform = transforms.ToTensor()
+transform = transforms.transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 # Loasd CIFAR-10 training dataset
 trainset = torchvision.datasets.CIFAR10(
@@ -17,8 +20,31 @@ trainloader = DataLoader(
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform)
 testloader = DataLoader(
-    testset, batch_size=4)
+    testset, batch_size=32)
 
 # Define the classes in CIFAR-10
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+device = "cpu"
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+NeuralNetwork = NeuralNetwork()
