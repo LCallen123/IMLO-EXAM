896d97a1fa2a49ae4f30394887e45312477121c8
Luke Callen <kgj508@york.ac.uk>
2025-05-10 20:47:15 +0100

    Changes to kernel,  conv layers, fc layers and testset

--- train.py
+++ train.py
@@ -7,9 +7,8 @@ import torchvision
 import torchvision.transforms as transforms
 
 # Transforms the dataset
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform = transforms.Compose([transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 # Loasd CIFAR-10 training dataset
 trainset = torchvision.datasets.CIFAR10(
@@ -21,11 +20,10 @@ trainloader = DataLoader(
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform)
 testloader = DataLoader(
-    testset, batch_size=32)
+    testset, batch_size=16)
 
 # Define the classes in CIFAR-10
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 device = "cpu"
 
@@ -33,23 +31,28 @@ class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
         
-        # Convolutional layers take 3 channel images for RGB
-        # with the second layer outputting 16 channels
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        # 3 convolutional layers which take 3 channels being RGB
+        # and output 128 channels with a kernel size of 3.
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.pool = nn.MaxPool2d(3, 2)
 
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = nn.Linear(128 * 3 * 3, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        # Flatten the output from the convolutional layers
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
         return x
 
 NeuralNet = NeuralNetwork()
@@ -80,7 +83,7 @@ def train_model(trainloader, loss_function, optimiser, epochs):
                 print(f'[Epoch {epoch + 1}] loss: {current_loss/3125:.3f}')
                 current_loss = 0
 
-    print('Finished Training')
+    print('training finished after %d epochs' % epochs)
 
 epochs = 200
 
