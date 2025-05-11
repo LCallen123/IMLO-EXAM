a4546fe8fae91a233dabc88bf9b471d41e6c682a
Luke Callen <kgj508@york.ac.uk>
2025-05-11 20:39:38 +0100

    .

--- train.py
+++ train.py
@@ -32,9 +32,9 @@ class NeuralNetwork(nn.Module):
         self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
         self.pool = nn.MaxPool2d(3, 2)
 
-        self.fc1 = nn.Linear(128 * 3 * 3, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 128)
+        self.fc1 = nn.Linear(128 * 3 * 3, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 128)
         self.fc4 = nn.Linear(128, 10)
 
     def forward(self, x):
